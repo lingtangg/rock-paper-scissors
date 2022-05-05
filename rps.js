109a50b85235cf18e8e34104fd6d2c40acf11d22
lingtangg <60630600+lingtangg@users.noreply.github.com>
2022-05-05 16:32:16 +1000

    Add running scoreboard and announcement of winner

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
 // Rock paper scissors logic
 const options = ["scissors", "paper", "rock"]
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return options[Math.floor(Math.random() * 3)]
@@ -17,12 +19,17 @@ function playRound(playerSelection, computerSelection) {
             }
             else if (computerSelection == "paper") {
                 result.textContent = "You win!";
+                playerScore++;
+                return playerScore;
                 break;
             }
             result.textContent = "You lose!";
+            computerScore++;
+            return computerScore;
         case "paper":
             if (computerSelection == "scissors") {
                 result.textContent = "You lose!";
+                computerScore++;
                 break;
             }
             else if (computerSelection == "paper") {
@@ -30,31 +37,59 @@ function playRound(playerSelection, computerSelection) {
                 break;
             }
             result.textContent = "You win!";
+            playerScore++;
         case "rock":
             if (computerSelection == "scissors") {
                 result.textContent = "You win!";
+                playerScore++;
                 break;
             }
             else if (computerSelection == "paper") {
                 result.textContent = "You lose!";
+                computerScore++;
                 break;
             }
-            result.textContent = "It's a tie!"
+            result.textContent = "It's a tie!";
+    }
+}
+
+function updateScore() {
+    playerScoreboard = document.getElementById('playerScore');
+    computerScoreboard = document.getElementById('computerScore');
+
+    playerScoreboard.innerHTML = `Player score: ${playerScore}`;
+    computerScoreboard.innerHTML = `Computer score: ${computerScore}`;
+}
+
+function gameEnd() {
+    game = document.getElementById('gameEnd');
+    if (playerScore == 5) {
+        game.innerHTML = "You win the game!";
+    }
+    else if (computerScore == 5) {
+        game.innerHTML = "The computer wins the game!";
     }
 }
 
 // Event listeners
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', function () {
-    playRound("rock", computerPlay())
+    playRound("rock", computerPlay());
+    updateScore();
+    gameEnd();
     }, false);
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', function () {
-    playRound("paper", computerPlay())
+    playRound("paper", computerPlay());
+    updateScore();
+    gameEnd();
     }, false);
 
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', function () {
-    playRound("scissors", computerPlay())
+    playRound("scissors", computerPlay());
+    updateScore();
+    gameEnd();
     }, false);
+
